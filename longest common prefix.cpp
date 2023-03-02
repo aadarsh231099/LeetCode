f32c496890da7f24a511ccb7940995bbeb603bbd
Aadarsh <aadarshhanu23@gmail.com>
2023-03-03 01:54:00 +0530

    easy

---
+++ longest common prefix.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/longest-common-prefix/
+//Longest Common Prefix
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); ++i) {
+            for (const auto& str : strs) {
+                if (i >= str.length() || str[i] != strs[0][i]) {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
