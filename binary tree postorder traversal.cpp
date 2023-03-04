481ebd7bdca2e728c30ea993f1b3ed22b0c5bd00
Aadarsh <aadarshhanu23@gmail.com>
2023-03-04 16:50:33 +0530

    easy

---
+++ binary tree postorder traversal.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/binary-tree-postorder-traversal/
+//Binary Tree Postorder Traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<pair<TreeNode *, bool>> s;
+        s.emplace(root, false);
+        while (!s.empty()) {
+            bool visited;
+            tie(root, visited) = s.top();
+            s.pop();
+            if (root == nullptr) {
+                continue;
+            }
+            if (visited) {
+                res.emplace_back(root->val);
+            } else {
+                s.emplace(root, true);
+                s.emplace(root->right, false);
+                s.emplace(root->left, false);
+            }
+        }
+        return res;
+    }
+};
