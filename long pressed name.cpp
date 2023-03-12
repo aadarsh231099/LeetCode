3b26c13fdfe8d9a8b149b03fba19118ca46035f7
Aadarsh <aadarshhanu23@gmail.com>
2023-03-12 12:23:21 +0530

    easy

---
+++ long pressed name.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/long-pressed-name/
+//Long Pressed Name
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0;
+        for (int j = 0; j < typed.length(); ++j) {
+            if (i < name.length() && name[i] == typed[j]) {
+                ++i;
+            } else if (j == 0 || typed[j] != typed[j - 1]) {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+};
