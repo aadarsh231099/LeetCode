063e5255afa4471b7670e16c5e6e474d3cab0670
Aadarsh <aadarshhanu23@gmail.com>
2023-03-20 12:14:24 +0530

    medium

---
+++ coin change ii.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/coin-change-ii/
+//Coin Change II
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (const auto& coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
