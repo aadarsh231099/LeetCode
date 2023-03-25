a06ea577036b9394fcf12aee45b6a99a39d36d47
Aadarsh <aadarshhanu23@gmail.com>
2023-03-25 12:02:23 +0530

    easy

---
+++ contains duplicate.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/contains-duplicate/
+//Contains Duplicate
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return unique(nums.begin(), nums.end()) != nums.end();
+    }
+};
