a947d59f6d811f89ea9d3bc07a8a829e39e1cc55
Aadarsh <aadarshhanu23@gmail.com>
2023-03-25 11:59:19 +0530

    easy

---
+++ find pivot index.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/find-pivot-index/
+//Find Pivot Index
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        const auto total = accumulate(nums.begin(), nums.end(), 0);
+        auto left_sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (left_sum == (total - left_sum - nums[i])) {
+                return i;
+            }
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
