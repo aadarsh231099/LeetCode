4882f5184afa996cc32fbe2a46bef364b4423024
Aadarsh <aadarshhanu23@gmail.com>
2023-03-27 12:21:01 +0530

    easy

---
+++ reverse linked list.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/reverse-linked-list/
+//Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* curr) {
+        ListNode* prev=NULL,*next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
