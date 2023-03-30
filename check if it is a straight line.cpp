18677181cf6b550c663ad1294a90bafd4fb0a463
Aadarsh <aadarshhanu23@gmail.com>
2023-03-30 17:00:35 +0530

    easy

---
+++ check if it is a straight line.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/check-if-it-is-a-straight-line/
+//Check If It Is a Straight Line
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        const auto& i = coordinates[0], &j = coordinates[1];
+        return all_of(coordinates.cbegin() + 2,
+                      coordinates.cend(),
+                      [&](const auto& k) {
+                          return (i[0] * j[1] - j[0] * i[1] + j[0] * k[1] - k[0] * j[1] + k[0] * i[1] - i[0] * k[1] == 0);
+                      }
+               );
+    }
+};
