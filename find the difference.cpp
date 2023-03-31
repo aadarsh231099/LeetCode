82fdb12e9f90ef84ba6083a0e952c4a7349d8dcc
Aadarsh <aadarshhanu23@gmail.com>
2023-03-31 22:02:04 +0530

    easy

---
+++ find the difference.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/find-the-difference/
+//Find the Difference
+class Solution {
+public:
+    char findTheDifference(string s, string t) 
+    {
+      for(int i=0;i<s.size();i++)
+		    t[i+1]+=t[i]-s[i]; 
+      return t[t.size()-1]; 
+    }
+};
