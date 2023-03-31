780e40c0a65f05200421b26b9f4db5385e67490e
Aadarsh <aadarshhanu23@gmail.com>
2023-03-31 12:38:13 +0530

    easy

---
+++ binary search.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/binary-search/
+//Binary Search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            auto mid = left + (right-left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
