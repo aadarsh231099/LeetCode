e4cbde875669db21a49c338a068bd9f10c9e5e2d
Aadarsh <aadarshhanu23@gmail.com>
2024-07-01 22:21:55 +0530

    easy

---
+++ EASY/three consecutive odds.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/three-consecutive-odds/
+//Three Consecutive Odds
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+};
