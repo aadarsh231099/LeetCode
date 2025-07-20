341b2a61767cefcd37ee8322d5ebf4adc42fadc3
Aadarsh <aadarshhanu23@gmail.com>
2025-07-20 17:12:26 +0530

    easy

--- EASY/concatenation-of-array.cpp
+++ EASY/concatenation-of-array.cpp
@@ -1,10 +1,24 @@
 //https://leetcode.com/problems/concatenation-of-array/description/
 //1929. Concatenation of Array
+//Sol 1
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
     vector<int> ans = nums;
     ans.insert(ans.end(), nums.begin(), nums.end()); 
     return ans;
+    }
+};
+
+//Sol 2
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(2*nums.size());
+        for(int i=0;i<nums.size();i++){
+            ans[i] = nums[i];
+            ans[i+nums.size()] = nums[i];
+        }
+        return ans;
     }
 };
