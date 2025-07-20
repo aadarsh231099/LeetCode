3a7680a53865a8bf1e4fee48fdf0a8a29fca1f6b
Aadarsh <aadarshhanu23@gmail.com>
2025-07-20 17:20:04 +0530

    easy

--- EASY/contains duplicate.cpp
+++ EASY/contains duplicate.cpp
@@ -1,9 +1,20 @@
 //https://leetcode.com/problems/contains-duplicate/
 //Contains Duplicate
+
+//Sol 1
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         return unique(nums.begin(), nums.end()) != nums.end();
+    }
+};
+
+// Sol 2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(),nums.end());
+        return nums.size()>s.size();
     }
 };
