056fa39576a5f407f80a52102df430cd4eb97e85
Aadarsh <aadarshhanu23@gmail.com>
2025-07-27 16:49:03 +0530

    easy

--- EASY/best time to buy and sell stock.cpp
+++ EASY/best time to buy and sell stock.cpp
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 //Best Time to Buy and Sell Stock
+
+//Sol1
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
@@ -16,5 +18,23 @@ public:
         }
 
         return r1;
+    }
+};
+
+//Sol2
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int profit = 0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else if(profit<prices[i]-min){
+                profit = prices[i]-min;
+            }
+        }
+        return profit;
     }
 };
